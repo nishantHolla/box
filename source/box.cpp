811662ed516ca79ddb6b433104beae52d312bc0a
nishantHolla <nishantholla19@gmail.com>
2023-08-23 15:28:37 +0530

    Add flipFile function

--- source/box.cpp
+++ source/box.cpp
@@ -1,11 +1,75 @@
 
 #include "box.hpp"
+#include <fstream>
 
 Box::Box() :
+	SIMPLE_FLIP_EXTS {".jpg", ".png", ".mp4", ".mp3", ".mkv"},
 	io (LOG_FILE)
 {
 	std::filesystem::remove(LOG_FILE);
 
+}
+
+void Box::singleByteFlip(std::fstream& _stream) {
+	char c;
+
+	_stream.seekg(std::ios::beg);
+	_stream.read(&c, 1);
+
+	c = c ^ FLIP_FILE_KEY;
+
+	_stream.seekp(std::ios::beg);
+	_stream.write(&c, 1);
+}
+
+void Box::multyByteFlip(std::fstream& _stream, const std::filesystem::path& _filePath) {
+	const int FILE_SIZE = std::filesystem::file_size(_filePath);
+	char *fileData = new char[FILE_SIZE];
+
+	_stream.seekg(std::ios::beg);
+	for (int i=0; i<FILE_SIZE; i++) {
+		char c;
+		_stream.read(&c, 1);
+		fileData[i] = (c ^ FLIP_FILE_KEY);
+	}
+
+	_stream.seekp(std::ios::beg);
+	for (int i=0; i<FILE_SIZE; i++) {
+		_stream.write(&(fileData[i]), 1);
+	}
+
+	delete[] fileData;
+}
+
+int Box::flipFile(const std::filesystem::path& _filePath) {
+
+	if (std::filesystem::is_regular_file(_filePath) == false) {
+		io.log( SisIO::messageType::error,
+			"Could not flip non existent file " + _filePath.string(),
+			"flipFile method"
+		);
+		return 1;
+	}
+
+	std::fstream FILE (_filePath, std::ios_base::binary | std::ios_base::out | std::ios_base::in);
+
+	if (!FILE || !FILE.good()) {
+		io.log( SisIO::messageType::error,
+			"Could not open file " + _filePath.string() + " for flipping",
+			"flipFile method"
+		);
+		return 2;
+	}
+
+	const std::string ext = _filePath.extension().string();
+	if ( std::find(SIMPLE_FLIP_EXTS.begin(), SIMPLE_FLIP_EXTS.end(), ext) != SIMPLE_FLIP_EXTS.end() )
+		singleByteFlip(FILE);
+
+	else
+		multyByteFlip(FILE, _filePath);
+
+	FILE.close();
+	return 0;
 }
 
 Box::~Box()
--- source/include/box.hpp
+++ source/include/box.hpp
@@ -5,20 +5,29 @@
 #include <bits/stdc++.h>
 #include <filesystem>
 #include <cstdlib>
+#include <vector>
 
 #include "sisIO.hpp"
 
 #define LOG_FILE_NAME "boxLog.txt"
+#define FLIP_FILE_KEY 128
 
 class Box {
 public:
 	Box();
 
+
 	~Box();
 
 private:
+
 	const std::filesystem::path HOME_DIR = std::filesystem::path(std::getenv("HOME"));
 	const std::filesystem::path LOG_FILE = HOME_DIR / LOG_FILE_NAME;
+	const std::vector<const char *> SIMPLE_FLIP_EXTS;
+
+	int flipFile(const std::filesystem::path& _filePath);
+	void singleByteFlip(std::fstream& _stream);
+	void multyByteFlip(std::fstream& _stream, const std::filesystem::path& _filePath);
 
 	SisIO io;
 };
